56cb9f27d23bd6b34c220d105300c478c1efd6b1
LeiWang1999 <wanglei21c@mails.ucas.ac.cn>
2024-03-02 07:28:32 +0000

    fix cublas issue

--- cuBLAS/cublas-benchmark/cublas_benchmark.cu
+++ cuBLAS/cublas-benchmark/cublas_benchmark.cu
@@ -30,9 +30,9 @@ std::vector<std::tuple<int, int, int, bool, bool>> inference_server_set = {
     std::make_tuple(1, 9216, 9216, false, true),
     std::make_tuple(1, 36864, 9216, false, true),
     std::make_tuple(1, 9216, 36864, false, true),
-    std::make_tuple(1, 8192, 8192, false, true),
     std::make_tuple(1, 22016, 8192, false, true),
     std::make_tuple(1, 8192, 22016, false, true),
+    std::make_tuple(1, 8192, 8192, false, true),
     std::make_tuple(1, 28672, 8192, false, true),
     std::make_tuple(1, 8192, 28672, false, true),
 
@@ -44,9 +44,9 @@ std::vector<std::tuple<int, int, int, bool, bool>> inference_server_set = {
     std::make_tuple(8192, 9216, 9216, false, true),
     std::make_tuple(8192, 36864, 9216, false, true),
     std::make_tuple(8192, 9216, 36864, false, true),
-    std::make_tuple(8192, 8192, 8192, false, true),
     std::make_tuple(8192, 22016, 8192, false, true),
     std::make_tuple(8192, 8192, 22016, false, true),
+    std::make_tuple(8192, 8192, 8192, false, true),
     std::make_tuple(8192, 28672, 8192, false, true),
     std::make_tuple(8192, 8192, 28672, false, true),
 
