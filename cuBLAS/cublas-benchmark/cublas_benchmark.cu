a96151a2854f03f45e0bf5d37e5a7218e67fa184
LeiWang <leiwang1999@outlook.com>
2024-03-25 22:52:07 -0400

    Update inference server set in cublas_benchmark.cu

--- cuBLAS/cublas-benchmark/cublas_benchmark.cu
+++ cuBLAS/cublas-benchmark/cublas_benchmark.cu
@@ -21,34 +21,34 @@
 // Vector saves m, n, k, a_t, b_t
 std::vector<std::tuple<int, int, int, bool, bool>> inference_server_set = {
 
-    // gemv
-    std::make_tuple(1, 16384, 16384, false, true),
-    std::make_tuple(1, 43008, 14336, false, true),
-    std::make_tuple(1, 14336, 14336, false, true),
-    std::make_tuple(1, 57344, 14336, false, true),
-    std::make_tuple(1, 14336, 57344, false, true),
-    std::make_tuple(1, 9216, 9216, false, true),
-    std::make_tuple(1, 36864, 9216, false, true),
-    std::make_tuple(1, 9216, 36864, false, true),
-    std::make_tuple(1, 22016, 8192, false, true),
-    std::make_tuple(1, 8192, 22016, false, true),
-    std::make_tuple(1, 8192, 8192, false, true),
-    std::make_tuple(1, 28672, 8192, false, true),
-    std::make_tuple(1, 8192, 28672, false, true),
+    // // gemv
+    // std::make_tuple(1, 16384, 16384, false, true),
+    // std::make_tuple(1, 43008, 14336, false, true),
+    // std::make_tuple(1, 14336, 14336, false, true),
+    // std::make_tuple(1, 57344, 14336, false, true),
+    // std::make_tuple(1, 14336, 57344, false, true),
+    // std::make_tuple(1, 9216, 9216, false, true),
+    // std::make_tuple(1, 36864, 9216, false, true),
+    // std::make_tuple(1, 9216, 36864, false, true),
+    // std::make_tuple(1, 22016, 8192, false, true),
+    // std::make_tuple(1, 8192, 22016, false, true),
+    // std::make_tuple(1, 8192, 8192, false, true),
+    // std::make_tuple(1, 28672, 8192, false, true),
+    // std::make_tuple(1, 8192, 28672, false, true),
 
     std::make_tuple(16384, 16384, 16384, false, true),
-    std::make_tuple(8192, 43008, 14336, false, true),
-    std::make_tuple(8192, 14336, 14336, false, true),
-    std::make_tuple(8192, 57344, 14336, false, true),
-    std::make_tuple(8192, 14336, 57344, false, true),
-    std::make_tuple(8192, 9216, 9216, false, true),
-    std::make_tuple(8192, 36864, 9216, false, true),
-    std::make_tuple(8192, 9216, 36864, false, true),
-    std::make_tuple(8192, 22016, 8192, false, true),
-    std::make_tuple(8192, 8192, 22016, false, true),
-    std::make_tuple(8192, 8192, 8192, false, true),
-    std::make_tuple(8192, 28672, 8192, false, true),
-    std::make_tuple(8192, 8192, 28672, false, true),
+    // std::make_tuple(8192, 43008, 14336, false, true),
+    // std::make_tuple(8192, 14336, 14336, false, true),
+    // std::make_tuple(8192, 57344, 14336, false, true),
+    // std::make_tuple(8192, 14336, 57344, false, true),
+    // std::make_tuple(8192, 9216, 9216, false, true),
+    // std::make_tuple(8192, 36864, 9216, false, true),
+    // std::make_tuple(8192, 9216, 36864, false, true),
+    // std::make_tuple(8192, 22016, 8192, false, true),
+    // std::make_tuple(8192, 8192, 22016, false, true),
+    // std::make_tuple(8192, 8192, 8192, false, true),
+    // std::make_tuple(8192, 28672, 8192, false, true),
+    // std::make_tuple(8192, 8192, 28672, false, true),
 
 };
 
@@ -340,7 +340,7 @@ int main(int argc, char **argv)
 
         int pad_kernels_count = 0;
 
-        for (const auto &problem : scaling_bitwidth)
+        for (const auto &problem : inference_server_set)
         {
             int m, n, k;
             bool a_t, b_t;
---
+++ figures/end2end_vLLM_llama_13b.py
@@ -0,0 +1,116 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+gemm_provider = ["BS8IN32OUT128", "BS1IN512OUT64", , "BS32IN64OUT512"]
+
+gemm_times_data = [
+    ('vLLM-GPTQ-INT4', (1.29201258833442, 9.02910487166567)),
+    ('vLLM-GPTQ-BitBLAS', (0.412775446, 1.088938882)),
+]
+
+providers = gemm_provider
+times_data = gemm_times_data
+_1x_baseline = "vLLM-GPTQ-INT4"
+_1x_baseline_times = dict(times_data)[_1x_baseline]
+
+# 计算其他方法相对加速比
+speed_up_data = []
+for label, times in times_data:
+    if label != _1x_baseline:
+        speed_up = [
+            p_i / t if t != 0 else 0 for p_i, t in zip(_1x_baseline_times, times)
+        ]
+        speed_up_data.append((label, speed_up))
+    else:
+        speed_up_data.append((label, [1.0] * len(times)))
+
+print(speed_up_data)
+# Data
+colers_sets = [
+    # nilu
+    # (20 / 255, 54 / 255, 95 / 255),
+    # (118 / 255, 162 / 255, 185 / 255),
+    # (191 / 255, 217 / 255, 229 / 255),
+    # (214 / 255, 79 / 255, 56 / 255),
+    # (112 / 255, 89 / 255, 146 / 255),
+    # white
+    (1, 1, 1),
+    # dori
+    # (169 / 255, 115 / 255, 153 / 255),
+    (248 / 255, 242 / 255, 236 / 255),
+    (214 / 255, 130 / 255, 148 / 255),
+    (243 / 255, 191 / 255, 202 / 255),
+    # coller
+    # (124 / 255, 134 / 255, 65 / 255),
+    # (185 / 255, 198 / 255, 122 / 255),
+    # (248 / 255, 231 / 255, 210 / 255),
+    # (182 / 255, 110 / 255, 151 / 255),
+    # (191 / 255, 217 / 255, 229 / 255),
+    # (214 / 255, 79 / 255, 56 / 255),
+    # (112 / 255, 89 / 255, 146 / 255),
+]
+hatch_patterns = ["x", "\\", "*", "o", "O", ".", "-", "+"]
+
+# Create an array for x-axis positions
+x = np.arange(len(providers))
+
+# Set the width of the bars
+bar_width = 0.21
+
+# Plotting
+fig, ax = plt.subplots(figsize=(9, 5))
+x = np.arange(len(providers))
+
+
+# Draw a vertical dashed line to separate the two data parts
+def get_inverse(a):
+    inverse = [1 / i for i in a]
+    return inverse
+
+# Create bars using a loop
+for i, (label, speedup) in enumerate(speed_up_data):
+    rec = ax.bar(
+        x + i * bar_width,
+        speedup,
+        bar_width,
+        label=label,
+        linewidth=0.8,
+        edgecolor="black",
+        hatch=hatch_patterns[i % 8],
+        color=colers_sets[i],
+    )
+
+# set y-limit
+max_speedup = max([max(speedup) for _, speedup in speed_up_data])
+ax.set_ylim(0, max_speedup * 1.3)
+# 调整图例位置和大小
+legend_fontsize = 14
+
+handles, labels = ax.get_legend_handles_labels()
+
+# 将图例放置在图表中间
+ax.legend(
+    handles,
+    labels,
+    loc="upper center",
+    bbox_to_anchor=(0.5, 1.0),
+    ncol=(len(labels) + 1) // 3 + 1,
+    fontsize=legend_fontsize,
+    frameon=False,
+)
+# X-axis and labels
+ax.set_xlabel("Configs", fontsize=18)
+ax.set_ylabel("Normalize Speedup vs GPTQ-CUDA", fontsize=20)
+ax.set_xticks(x + len(speed_up_data) * bar_width / len(times_data))
+ax.set_xticklabels(providers)
+ax.grid(axis="y", linestyle="--", linewidth=0.5)
+
+# disable grid
+ax.grid(False)
+
+# add a title
+plt.title("LLAMA-13B-INT4 Inference Speedup on A100 (FP16)", fontsize=18)
+
+# Save the plot to a file
+plt.savefig("pdf/end2end_llama_13b_auto_gptq.pdf")
+plt.savefig("png/end2end_llama_13b_auto_gptq.png", bbox_inches='tight', transparent=False, dpi=180)
